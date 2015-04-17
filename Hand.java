7a3340b1de7e89d5d48aaf17fe6d9265149d67b2
imamathgeek <imamathgeek@gmail.com>
2015-04-17 18:49:22 -0400

    Hand.java and War.class

--- Hand.java
+++ Hand.java
@@ -4,10 +4,10 @@ Homework 10: Final Project
 Hand class
 */
 
-public class Hand extends CardPile
+public class Hand extends Deck
 {
    public static final int START_HAND_NUM=27;
-   private CardPile hand1,hand2;
+   private CardPile hand;
    private Deck deck;
    
    /**
@@ -16,19 +16,28 @@ public class Hand extends CardPile
    public Hand()
    {
       //create two hands and set the num of cards in each to START_HAND_NUM
-      hand1=new CardPile();
-      hand2=new CardPile();
-      hand1.setNumCards(START_HAND_NUM);
-      hand2.setNumCards(START_HAND_NUM);
-      
+      hand=new CardPile();
+      hand.setNumCards(START_HAND_NUM);
+   }
+   /**
+      dealHand method deals the deck into how every many cards need to be in the hand
+      @return CardPile object The hand
+   */   
+   public CardPile dealHand()
+   {
       // create new deck
       deck=new Deck ();
       
-      //deal 27 cards into each hand
+      deck.shuffle();
+      
+      //deal START_HAND_NUM cards into hand
       for (int i=1;i<=START_HAND_NUM;i++)
       {
-         hand1.add(deck.dealCard()); //deal to hand 1
-         hand2.add(deck.dealCard()); //deal to hand 2
+         hand.add(deck.dealCard()); //deal to hand 1
       }
+      
+      return hand;
    }
+   
+   
 }
